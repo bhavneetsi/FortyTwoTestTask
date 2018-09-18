a9196d4ddf9201fe9e309135a85e40e0dc9cee97
tk <bhavneetsi@gmail.com>
2018-09-19 00:02:01 +0530

    ticket:3 adding models views and middleware and changing settings accordingly

--- apps/fortytwoapps/views.py
+++ apps/fortytwoapps/views.py
@@ -1,5 +1,7 @@
-from .models import Contact
-from django.views.generic import TemplateView
+from .models import Contact, Request
+from django.views.generic import TemplateView, ListView
+from django.http import HttpResponse
+from json import dumps
 # Create your views here.
 
 
@@ -10,3 +12,26 @@ class Index(TemplateView):
         context = super(Index, self).get_context_data(**kwargs)
         context['contact'] = Contact.objects.first()
         return context
+
+
+class Requests(ListView):
+    """
+    view to handle request page
+    """
+    model = Request
+    template_name = 'fortytwoapps/requests.html'
+    queryset = Request.objects.values()[:10]
+
+    def get(self, request, *args, **kwargs):
+        requestlist = Request.objects.values()[:10]
+        for request in requestlist:
+            request['time'] = request['time'].strftime('%b %d %Y %H:%M:%S')
+        if self.request.is_ajax():
+            if self.request.GET.get('focus') == 'true':
+                Request.objects.filter(viewed=False).update(viewed=True)
+
+            context = {'new_requests': Request.objects.filter(viewed=False)
+                       .count(), 'request_list': list(requestlist)}
+            return HttpResponse(dumps(context),
+                                content_type="application/json")
+        return super(Requests, self).get(request, *args, **kwargs)
