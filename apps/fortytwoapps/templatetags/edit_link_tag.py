b1ccb8cb21e53024a0ecb6854d77c06922d40686
tk <bhavneetsi@gmail.com>
2018-09-22 22:08:00 +0530

    ticket:8 added edit_link templatetag

---
+++ apps/fortytwoapps/templatetags/edit_link_tag.py
@@ -0,0 +1,18 @@
+from django.core.urlresolvers import reverse
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def edit_link(obj,*args):
+    """A template tag which accepts any object and renders a link to its
+    admin edit page"""
+    try:
+        arguments = args
+        view_name = 'admin:{}_change'.format(obj._meta.db_table)
+        return reverse(view_name, args=arguments)
+    except AttributeError:
+        raise template.TemplateSyntaxError(
+            "{} isn't correct object".format(obj))
